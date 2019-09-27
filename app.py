ed7096df3f1d8300a818582658156b9ca013339d
cjj <kewellcjj@gmail.com>
2019-09-26 23:24:18 -0400

    solve issue 1

--- app.py
+++ app.py
@@ -1,5 +1,6 @@
-from flask import Flask, render_template
-from flask_flatpages import FlatPages
+from flask import Flask, render_template, render_template_string
+from flask_flatpages import FlatPages, pygments_style_defs
+import markdown
 
 DEBUG = True
 FLATPAGES_AUTO_RELOAD = DEBUG
@@ -8,6 +9,18 @@ FLATPAGES_EXTENSION = '.md'
 app = Flask(__name__)
 app.config.from_object(__name__)
 pages = FlatPages(app)
+
+def my_renderer(text):
+    """Inject the markdown rendering into the jinga template"""
+    rendered_body = render_template_string(text)
+    pygmented_body = markdown.markdown(rendered_body, extensions=['codehilite', 'fenced_code', 'tables'])
+    return pygmented_body
+
+app.config.update({
+    'FLATPAGES_EXTENSION': ['.md', '.markdown'],
+    'FLATPAGES_MARKDOWN_EXTENSIONS': ['codehilite', 'fenced_code', 'tables'],
+    'FLATPAGES_HTML_RENDERER': my_renderer
+})
 
 @app.route('/')
 def index():
@@ -22,6 +35,10 @@ def page(path):
 def tag(tag):
     tagged = [p for p in pages if tag in p.meta.get('tags', [])]
     return render_template('tag.html', pages=tagged, tag=tag)
-    
+
+@app.route('/pygments.css')
+def pygments_css():
+    return pygments_style_defs('tango'), 200, {'Content-Type': 'text/css'}
+
 if __name__ == "__main__":
     app.run()
