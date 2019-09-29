716ffc9c21174c694ff0c1f2a4e504bc515f321c
cjj <kewellcjj@gmail.com>
2019-09-28 23:04:06 -0400

    unified template, adding side bar on all post pages

--- app.py
+++ app.py
@@ -39,9 +39,7 @@ app.config.update({
     'FLATPAGES_HTML_RENDERER': my_renderer,
 })
 
-@app.route('/')
-def index():
-    latest = sorted(pages, reverse=True, key=lambda p: p.meta['date'])
+def index_summary(pages):
     #collect all tags
     tags = {}
     for p in pages:
@@ -54,28 +52,59 @@ def index():
     #collect publish dates
     dates = {}
     for p in pages:
-        mmyyyy = p.meta['date'].strftime("%B %Y")
+        mmyyyy = p.meta.get('date','').strftime("%B %Y")
         if mmyyyy not in dates:
             dates[mmyyyy] = 1
         else:
             dates[mmyyyy] += 1
     sorted_dates = sorted(dates.items(), reverse=True, key=lambda x: x[1])
-    return render_template('index.html', pages=latest, tags=sorted_tags, dates=sorted_dates)
+
+    return sorted_tags, sorted_dates
+
+
+sorted_tags, sorted_dates = index_summary(pages)
+
+@app.route('/')
+def index():
+    # #collect all tags
+    # tags = {}
+    # for p in pages:
+    #     for tag in p.meta.get('tags', []):
+    #         if tag not in tags:
+    #             tags[tag] = 1
+    #         else:
+    #             tags[tag] += 1
+    # sorted_tags = sorted(tags.items(), reverse=True, key=lambda x: x[1])
+    # #collect publish dates
+    # dates = {}
+    # for p in pages:
+    #     mmyyyy = p.meta.get('date','').strftime("%B %Y")
+    #     if mmyyyy not in dates:
+    #         dates[mmyyyy] = 1
+    #     else:
+    #         dates[mmyyyy] += 1
+    # sorted_dates = sorted(dates.items(), reverse=True, key=lambda x: x[1])
+
+    latest = sorted(pages, reverse=True, key=lambda p: p.meta['date'])
+    return render_template('index.html', pages=latest, tags=sorted_tags, dates=sorted_dates, list_title="Recent Posts")
 
 @app.route('/<path:path>/')
 def page(path):
     page = pages.get_or_404(path)
-    return render_template('page.html', page=page)
+    # return render_template('page.html', page=page)
+    return render_template('index.html', pages=[page], tags=sorted_tags, dates=sorted_dates, list_title="")
 
 @app.route('/tag/<string:tag>/')
 def tag(tag):
     tagged = [p for p in pages if tag in p.meta.get('tags', [])]
-    return render_template('tag.html', pages=tagged, tag=tag)
+    tagged = sorted(tagged, reverse=True, key=lambda p: p.meta['date'])
+    return render_template('index.html', pages=tagged, tags=sorted_tags, dates=sorted_dates, list_title="Recent Posts Tagged "+tag.title())
 
 @app.route('/archive/<string:date>/')
 def archive(date):
     archive = [p for p in pages if date == p.meta.get('date', []).strftime("%B %Y")]
-    return render_template('archive.html', pages=archive, date=date)
+    archive = sorted(archive, reverse=True, key=lambda p: p.meta['date'])
+    return render_template('index.html', pages=archive, tags=sorted_tags, dates=sorted_dates, list_title="Posts in "+date.title())
 
 @app.route('/pygments.css')
 def pygments_css():
